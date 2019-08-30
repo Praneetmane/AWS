7ec192c0a280059a87c96e9c1ed0aa6709e25269
Praneet Mane <praneet@ampool.io>
2019-08-30 12:12:04 +0530

    stopped aws instance

---
+++ stopped-instance/Stopped-Instance.py
@@ -0,0 +1,35 @@
+import boto3
+import logging
+
+#setup simple logging for INFO
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+#define the connection
+ec2 = boto3.resource('ec2')
+# open connection to ec2
+#conn = get_ec2_conn()
+
+# get a list of all instances
+# all_instances = [i for i in ec2.instances.all()]
+# get list of all running instances
+
+all_instances = [i for i in ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])]
+
+def lambda_handler():
+
+    #instances = ec2.instances.filter(Filters=filters)
+    # get instances with filter of running + with tag `Name`
+    instances = [i for i in ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}, {'Name':'tag:autoshutdown', 'Values': ['false']}])]
+    #print instances
+
+    # make a list of filtered instances IDs `[i.id for i in instances]`
+    # Filter from all instances the instance that are not in the filtered list
+    instances_to_stop = [to_stop for to_stop in all_instances if to_stop.id not in [i.id for i in instances]]
+
+    # run over your `instances_to_stop` list and stop each one of them
+    for instance in instances_to_stop:
+        print instance.stop()
+
+if __name__ == '__main__':
+    lambda_handler()
